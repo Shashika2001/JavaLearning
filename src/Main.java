0bd24524fb271a8d330f919a73c9b5cd9cc83ded
shashika2001 <ekanayakespm@gmail.com>
2024-06-19 01:03:15 +0530

    Date

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        long viesCount=3_123_456_789L;
+        Date now = new Date();
+        System.out.println(now);
     }
 }
