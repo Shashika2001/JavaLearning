196cc5812c1344847eb46bf3d83a0fb50e285af9
shashika2001 <ekanayakespm@gmail.com>
2024-06-20 10:23:02 +0530

    arrays sorting array

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
+import java.util.Arrays;
 import java.util.Date;
 import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-      String message="Hello World";
-        System.out.println(message);
+     int [] numbers = {1,3,2,4,5};
+     Arrays.sort(numbers);
+        System.out.println(numbers.length);
+
+
+        System.out.println( Arrays.toString(numbers));
     }
 }
